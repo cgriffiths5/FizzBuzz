a3396d147cdbf6c6f5180c69f90f7fad9ca29266
Chris Griffiths <chrisgriffiths635@outlook.com>
2024-05-21 20:11:57 +0100

    Added data to check output

--- FizzBuzzSolid/Program.cs
+++ FizzBuzzSolid/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace FizzBuzzSolid
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzSolid
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FizzBuzzController fizzBuzzController = new FizzBuzzController();
+
+            Console.WriteLine("Fizz Buzz Up To 10");
+
+            fizzBuzzController.PrintList(10);
+
+            Console.WriteLine("Fizz Buzz Up To 100");
+
+            fizzBuzzController.PrintList(100);
+
+
         }
     }
 }
